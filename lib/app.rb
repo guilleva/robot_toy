e8c00fee6014f3c93e42c8d436cb2945f48a4035
Guillermo Vargas <guilleva@gmail.com>
2016-04-22 19:48:36 -0600

    App can recognize the different commands

--- lib/app.rb
+++ lib/app.rb
@@ -1,13 +1,56 @@
+require 'robot'
+
 # Main app class
 class App
+  ROTATION_COMMANDS = %w(LEFT RIGHT)
+
+  MOVE_COMMAND = 'MOVE'
+
+  REPORT_COMMAND = 'REPORT'
+
+  QUIT_COMMAND = 'QUIT'
+
+  PLACE_REGEXP = /\A
+                  PLACE\s*
+                  (?<x>\d+)\s*,\s*
+                  (?<y>\d+)\s*,\s*
+                  (?<face>NORTH|SOUTH|EAST|WEST)\s*
+                  \z/x
+
+  attr_reader :robot
+
+  def initialize
+    @table = Table.new
+    @robot = Robot.new(@table)
+  end
+
   def run
     display_greeting_message
     while (cmd = gets.chomp)
-      break if cmd == 'QUIT'
+      break if cmd == QUIT_COMMAND
+      process_command cmd
+    end
+  end
+
+  def process_command(cmd)
+    case
+    when cmd == MOVE_COMMAND
+      @robot.move
+    when cmd == REPORT_COMMAND
+      output @robot.to_s
+    when ROTATION_COMMANDS.include?(cmd)
+      @robot.rotate(cmd)
+    else
+      attrs = PLACE_REGEXP.match(cmd)
+      @robot.place(attrs[:x].to_i, attrs[:y].to_i, attrs[:face]) if attrs
     end
   end
 
   def display_greeting_message
-    puts 'Hello!!!'
+    output 'Hello!!!'
+  end
+
+  def output(message)
+    puts message
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,4 +9,62 @@ describe App do
       subject.display_greeting_message
     end
   end
+
+  describe '#process_command' do
+    describe 'MOVE command' do
+      it 'calls "Robot#move"' do
+        expect(subject.robot).to receive(:move)
+        subject.process_command('MOVE')
+      end
+    end
+
+    describe 'LEFT command' do
+      it 'calls "Robot#rotate"' do
+        expect(subject.robot).to receive(:rotate).with('LEFT')
+        subject.process_command('LEFT')
+      end
+    end
+
+    describe 'RIGHT command' do
+      it 'calls "Robot#rotate"' do
+        expect(subject.robot).to receive(:rotate).with('RIGHT')
+        subject.process_command('RIGHT')
+      end
+    end
+
+    describe 'REPORT command' do
+      it 'calls "Robot#report"' do
+        subject.robot.place(0, 0, 'NORTH')
+        expect(subject).to receive(:output).with('0,0,NORTH')
+        subject.process_command('REPORT')
+      end
+    end
+
+    describe 'PLACE command' do
+      it 'calls "Robot#place"' do
+        expect(subject.robot).to receive(:place).with(1, 3, 'SOUTH')
+        subject.process_command('PLACE 1,3,SOUTH')
+      end
+
+      it 'accepts spaces within the parameters "Robot#place"' do
+        expect(subject.robot).to receive(:place).with(1, 3, 'SOUTH')
+          .exactly(5).times
+        subject.process_command('PLACE 1 ,3,SOUTH')
+        subject.process_command('PLACE 1, 3,SOUTH')
+        subject.process_command('PLACE 1,3 ,SOUTH')
+        subject.process_command('PLACE 1,3, SOUTH')
+        subject.process_command('PLACE 1,3,SOUTH ')
+      end
+
+      it 'ignores it when invalid params are given' do
+        expect(subject.robot).not_to receive(:place)
+
+        subject.process_command('PLACE x,3,SOUTH')
+
+        subject.process_command('PLACE 1,x,SOUTH')
+
+        subject.process_command('PLACE 1,x,SOUTHx')
+      end
+    end
+  end
 end
